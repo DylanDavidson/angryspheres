be459672cf49c9d6b28338995037c0b16bf70b85
Dylan Davidson <ddavidson621@gmail.com>
2015-09-13 17:53:49 -0400

    Adds grass and wood textures.

--- src/cube.js
+++ src/cube.js
@@ -3,13 +3,16 @@
 //
 var Cube = function(x, y, z)
 {
-  Cube.COLOR = 0x27ae60;
+  Cube.COLOR = 0xBC8151;
+  Cube.TEXTURE = THREE.ImageUtils.loadTexture("textures/wood.png");
+  Cube.TEXTURE.wrapS = THREE.RepeatWrapping;
+  Cube.TEXTURE.wrapT = THREE.RepeatWrapping;
 
   ThreeObject.call(this);
 
   // Create the BoxGeometry and material and add to scene
   this.geometry = new THREE.BoxGeometry(3, 3, 10);
-  this.material = new THREE.MeshBasicMaterial({ color: Cube.COLOR });
+  this.material = new THREE.MeshLambertMaterial({ map: Cube.TEXTURE });
   this.object = new Physijs.BoxMesh(this.geometry, this.material, 20);
 
   this.object.castShadow = true;
--- src/floor.js
+++ src/floor.js
@@ -3,13 +3,19 @@
 //
 var Floor = function()
 {
-  Floor.COLOR = 0x663300;
+  Floor.COLOR = new THREE.Color().setHSL( 0.3, 0.75, ( 10 / 15 ) * 0.4 + 0.1 );
+  Floor.TEXTURE = THREE.ImageUtils.loadTexture("textures/grass.png");
+  Floor.TEXTURE.wrapS = THREE.RepeatWrapping;
+  Floor.TEXTURE.wrapT = THREE.RepeatWrapping;
+  Floor.TEXTURE.repeat.set(10,100);
 
   ThreeObject.call(this);
 
   // Create the BoxGeometry and material and add to scene
   this.geometry = new THREE.BoxGeometry(30,200,1);
-  this.material = new THREE.MeshBasicMaterial({ color: Floor.COLOR });
+  this.material = new THREE.MeshBasicMaterial({
+    map: Floor.TEXTURE
+  });
   this.object = new Physijs.BoxMesh(this.geometry, this.material, 0);
 
   this.object.receiveShadow = true;
